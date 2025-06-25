beb7d150b6e1294123e471cea26cfb0a70a647a0
nath <ncart222@outlook.com>
2025-06-25 21:12:27 +0100

    First Commit: backend and frontend for pubfinder

---
+++ PubFinder.API/Controllers/PubsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PubFinder.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PubsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var pubs = new[]
+            {
+                new { id = 1, name = "The Crown and Anchor", location = "London", isOpen = true, rating = 4.6 },
+                new { id = 2, name = "The Old Dog", location = "Manchester", isOpen = false, rating = 4.2 }
+            };
+
+            return Ok(pubs);
+        }
+    }
+}
